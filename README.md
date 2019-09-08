971c24a26eb9109c55ee86ea022b74853ca838ab
Andrew McOlash <amcolash@gmail.com>
2019-09-08 01:16:32 -0700

    outlook mostly working, add icon, etc

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # status-scripts
 Scripts to provide status of various things in a terminal output, good for i3/genmon
+
+Icons from [Spotify](https://developer.spotify.com/branding-guidelines/) and [IconFinder](https://www.iconfinder.com/icons/285670/calendar_icon)
---
+++ outlook.js
@@ -0,0 +1,187 @@
+const fs = require('fs');
+const querystring = require('querystring');
+const axios = require('axios');
+const express = require('express');
+const FileStore = require('fs-store').FileStore;
+const moment = require('moment');
+
+require('dotenv').config();
+const store = new FileStore('data.json');
+
+const CLIENT_ID = process.env.MICROSOFT_CLIENT_ID;
+const CLIENT_SECRET = process.env.MICROSOFT_CLIENT_SECRET;
+const PLUGIN = process.env.PLUGIN;
+const PORT = process.env.PORT || 8889;
+const REDIRECT = `http://localhost:${PORT}/callback`;
+const ERRORS = true;
+const scopes = 'offline_access calendars.read';
+
+const app = express();
+app.listen(PORT);
+console.log(`Listening on ${PORT}`);
+
+let ACCESS, REFRESH;
+REFRESH = store.get('microsoftRefresh');
+
+// setIntervalImmediately(() => {
+getEvents();
+// }, 5000);
+
+app.get('/', (req, res) => {
+  getEvents(res);
+});
+
+app.get('/login', (req, res) => {
+  res.redirect('https://login.microsoftonline.com/common/oauth2/v2.0/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: CLIENT_ID,
+      scope: scopes,
+      redirect_uri: REDIRECT
+    })
+  );
+});
+
+app.get('/callback', (req, res) => {
+  console.log(req.query);
+  const code = req.query.code || null;
+
+  const data = {
+    code: code,
+    redirect_uri: REDIRECT,
+    grant_type: 'authorization_code',
+    scope: scopes,
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET
+  };
+
+  axios.post('https://login.microsoftonline.com/common/oauth2/v2.0/token', querystring.stringify(data)).then(response => {
+    ACCESS = response.data.access_token;
+    REFRESH = response.data.refresh_token;
+    store.set('microsoftRefresh', REFRESH);
+
+    if (res) res.redirect('/');
+  }).catch(err => {
+    if (ERRORS) console.error(err.response);
+    const info = 'Error getting access token from authorization code'
+    res.status(500).send(info);
+    updatePlugin(info);
+  });
+});
+
+function getAccess(res, cb) {
+  const data = {
+    grant_type: 'refresh_token',
+    refresh_token: REFRESH,
+    scope: scopes,
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET
+  };
+  axios.post('https://login.microsoftonline.com/common/oauth2/v2.0/token', querystring.stringify(data)).then(response => {
+    ACCESS = response.data.access_token;
+    if (res) res.redirect('/');
+    if (cb) cb();
+  }).catch(err => {
+    if (ERRORS) console.error(err.response);
+    const info = 'Error getting access token from refresh token';
+    if (res) res.status(500).send(info);
+    updatePlugin(info);
+  })
+}
+
+function getEvents(res) {
+  if (ACCESS) {
+    if (res) res.send('TODO');
+
+    axios.get('https://graph.microsoft.com/v1.0/me/calendars', {
+      headers: {
+        Authorization: 'Bearer ' + ACCESS
+      }
+    }).then(response => {
+      const startWeek = moment().startOf('week');
+      const endWeek = moment().endOf('week');
+
+      const data = {
+        startdatetime: startWeek.format(),
+        enddatetime: endWeek.format(),
+        $top: 50 // 50 items per page
+      };
+
+      let p = response.data.value.map(calendar => {
+        return axios.get(`https://graph.microsoft.com/v1.0/me/calendars/${calendar.id}/calendarview?` + querystring.stringify(data), {
+          headers: {
+            Authorization: 'Bearer ' + ACCESS
+          }
+        });
+      });
+
+      axios.all(p).then(data => {
+        let events = [];
+        data.forEach(c => {
+          c.data.value.forEach(e => {
+            events.push({
+              ...e,
+              start: moment.utc(e.start.dateTime),
+              end: moment.utc(e.end.dateTime)
+            });
+          });
+        });
+
+        // Sort
+        events = events.sort((a, b) => {
+          return a.start - b.start
+        }).filter(e => e.showAs === 'busy');
+
+        events.forEach(e => console.log(e.subject));
+      }).catch(err => {
+        if (ERRORS) console.error(err.response);
+        const info = 'Couldn\'t get upcoming events';
+        updatePlugin(info);
+        if (res) res.status(500).send(info);
+      })
+    }).catch(err => {
+      console.error(err.response);
+    });
+  } else if (REFRESH) {
+    console.log('Getting access token');
+    getAccess(res, () => getEvents());
+  } else {
+    if (res) res.redirect('/login');
+    const info = `Not logged in, please visit http://localhost:${PORT}`;
+    updatePlugin(info);
+  }
+}
+
+function updatePlugin(info, data) {
+  console.log(info);
+  return;
+
+  try {
+    switch (PLUGIN) {
+      case 'genmon':
+        let file;
+        if (data && data.is_playing) {
+          file = `<img>${__dirname}/icons/spotify.png</img><txt> ${info}</txt><tool>Spotify playing on ${data.device.name}\n${info}</tool>`;
+        } else if (data) {
+          file = `<img>${__dirname}/icons/spotify_disabled.png</img><txt> </txt><tool>Nothing Currently Playing</tool>`;
+        } else {
+          file = info;
+        }
+        // Fix issues with & character
+        file = file.replace(/&/g,'+');
+
+        fs.writeFileSync('./info', file);
+        break;
+      default:
+        console.log(info);
+        break;
+    }
+  } catch (err) {
+    if (ERRORS) console.error(err);
+  }
+}
+
+function setIntervalImmediately(func, interval) {
+  func();
+  return setInterval(func, interval);
+}
